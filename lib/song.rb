2d1e7e5c905db0c66427aaa3b2e5792352edd977
github username <github email address>
2018-06-06 21:38:27 -0400

    Song class internal functions

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,65 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    #binding.pry
+    song = self.create
+    song.name = song_name
+    song.save
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song.save
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect {|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    song = self.find_by_name(song_name)
+
+    if song == nil
+      self.create_by_name(song_name)
+    else
+      song
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort {|a,b| a.name <=> b.name}.uniq
+  end
+
+  def self.new_from_filename(file)
+    song = self.create
+    song_data = file.split(" - ")
+    song.name = song_data[1].split(".")[0]
+    song.artist_name = song_data[0]
+    song.save
+    song
+  end
+
+  def self.create_from_filename(file)
+    song = self.create
+    song_data = file.split(" - ")
+    song.name = song_data[1].split(".")[0]
+    song.artist_name = song_data[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear 
+  end
+
 end
